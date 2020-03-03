e9804b6e2bff7803952282323f1f0e3160568f83
RodrigoFulge <61611308+RodrigoFulge@users.noreply.github.com>
2020-03-02 22:10:10 -0600

    Added files for the Python Introduction program
    Both the .py file and its description PDF were added

---
+++ PythonIntro.py
@@ -0,0 +1,21 @@
+FIRST_NAME = "Luis Rodrigo"
+LAST_NAME = "Quintanar Ruiz"
+AGE = "18"
+HEIGHT = "1.7m"
+DRIVERS_LIC = True
+WEIGHT = '80KG'
+
+number = '7731387762'
+
+print('{:20}{} {}'.format('Name: ', FIRST_NAME,LAST_NAME))
+
+print('{:20}{}'.format('Age: ', AGE))
+
+print('{:20}{}'.format('Height: ', HEIGHT))
+
+
+print('{:20}{}'.format('Weight: ', WEIGHT))
+
+print('{:25}{}'.format("Has Driver's license?: ",DRIVERS_LIC))
+
+print('{:20}{}'.format('Phone Number: ', number))
